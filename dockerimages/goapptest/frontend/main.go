79ab437fc7c88327cc425f9961ed8878868b72e0
Liran Cohen <liran@rct.co.il>
2018-10-06 15:30:41 +0300

    adding a frontend container

---
+++ dockerimages/goapptest/frontend/main.go
@@ -0,0 +1,38 @@
+// Package of main frontend
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+// The ContactDetails struct
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("upload.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		details := ContactDetails{
+			Email:   r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		// do something with details
+		_ = details
+
+		tmpl.Execute(w, struct{ Success bool }{true})
+	})
+
+	http.ListenAndServe(":8088", nil)
+}
--- dockerimages/goapptest/gottestgin/main.go
+++ dockerimages/goapptest/gottestgin/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -14,21 +13,30 @@ type transformedTodo struct {
 }
 
 type controlSignal struct {
-	ControlSignal string `json:"controlSignal"`
-	State         bool   `json:"state"`
+	Signal string `json:"signal"`
+	State  bool   `json:"state"`
 }
 
-func createTodo(c *gin.Context) {
-	fmt.Println("testing")
-}
+// func createTodo(c *gin.Context) {
+// 	fmt.Println("testing")
+// }
 func fetchAllTodo(c *gin.Context) {
 	todo := transformedTodo{ID: 5, Title: "test", Completed: true}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": todo})
 }
 
 func initializeProcess(c *gin.Context) {
-	todo := controlSignal{ContrlSignal: "Starting process", Title: "test", Completed: true}
+	todo := controlSignal{Signal: "Starting process", State: true}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": todo})
+}
+
+func populateDB(c *gin.Context) {
+	todo := controlSignal{Signal: "Populate DB", State: true}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": todo})
+}
+
+func drawForm(c *gin.Context) {
+
 }
 
 func fetchSingleTodo(c *gin.Context) {}
@@ -37,14 +45,15 @@ func deleteTodo(c *gin.Context)      {}
 
 func main() {
 	router := gin.Default()
-	v1 := router.Group("/api/v1/")
+	v1 := router.Group("/")
 	{
-		v1.POST("/", createTodo)
+		// v1.POST("/", createTodo)
 		v1.GET("/", fetchAllTodo)
 		v1.GET("/init", initializeProcess)
-		v1.GET("/:id", fetchSingleTodo)
-		v1.PUT("/:id", updateTodo)
-		v1.DELETE("/:id", deleteTodo)
+		v1.GET("/populate", populateDB)
+		// v1.GET("/:id", fetchSingleTodo)
+		// v1.PUT("/:id", updateTodo)
+		// v1.DELETE("/:id", deleteTodo)
 	}
 	router.Run()
 }
