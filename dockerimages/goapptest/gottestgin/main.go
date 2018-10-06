aca2fc98ffec5df6b0fceb6590a9d92190c1bcc8
Liran Cohen <liran@rct.co.il>
2018-10-06 11:38:52 +0300

    added Json functionality to go routine

--- dockerimages/goapptest/gottestgin/main.go
+++ dockerimages/goapptest/gottestgin/main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-func createTodo(c *gin.Context)      {}
-func fetchAllTodo(c *gin.Context)    {}
+type transformedTodo struct {
+	ID        uint   `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+type controlSignal struct {
+	ControlSignal string `json:"controlSignal"`
+	State         bool   `json:"state"`
+}
+
+func createTodo(c *gin.Context) {
+	fmt.Println("testing")
+}
+func fetchAllTodo(c *gin.Context) {
+	todo := transformedTodo{ID: 5, Title: "test", Completed: true}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": todo})
+}
+
+func initializeProcess(c *gin.Context) {
+	todo := controlSignal{ContrlSignal: "Starting process", Title: "test", Completed: true}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": todo})
+}
+
 func fetchSingleTodo(c *gin.Context) {}
 func updateTodo(c *gin.Context)      {}
 func deleteTodo(c *gin.Context)      {}
 
 func main() {
 	router := gin.Default()
-	v1 := router.Group("/api/v1/todos")
+	v1 := router.Group("/api/v1/")
 	{
 		v1.POST("/", createTodo)
 		v1.GET("/", fetchAllTodo)
+		v1.GET("/init", initializeProcess)
 		v1.GET("/:id", fetchSingleTodo)
 		v1.PUT("/:id", updateTodo)
 		v1.DELETE("/:id", deleteTodo)
