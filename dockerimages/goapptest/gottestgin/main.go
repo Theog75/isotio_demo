a1b23754dda49e152183507e112378e433cfdff1
Liran Cohen <liran@rct.co.il>
2018-10-06 11:10:16 +0300

    using Gin Gonic

---
+++ dockerimages/goapptest/gottestgin/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func createTodo(c *gin.Context)      {}
+func fetchAllTodo(c *gin.Context)    {}
+func fetchSingleTodo(c *gin.Context) {}
+func updateTodo(c *gin.Context)      {}
+func deleteTodo(c *gin.Context)      {}
+
+func main() {
+	router := gin.Default()
+	v1 := router.Group("/api/v1/todos")
+	{
+		v1.POST("/", createTodo)
+		v1.GET("/", fetchAllTodo)
+		v1.GET("/:id", fetchSingleTodo)
+		v1.PUT("/:id", updateTodo)
+		v1.DELETE("/:id", deleteTodo)
+	}
+	router.Run()
+}
