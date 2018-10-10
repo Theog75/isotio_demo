21c2b10bdda1b971178ac111a285f906e14d39a8
Liran Cohen <liran@weopen.co.il>
2018-10-10 09:34:37 +0300

    before pull

--- dockerimages/goapptest/main.go
+++ dockerimages/goapptest/main.go
@@ -10,6 +10,9 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		if html.EscapeString(r.URL.Path) == "/liran" {
+			fmt.Println("Correct")
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":8089", nil))
