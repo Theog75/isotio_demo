c539ae630cffd7820763b65b3f45df0cf1d7a20c
Liran Cohen <liran@weopen.co.il>
2018-10-24 21:36:56 +0300

    latest update

--- titlesuploader/main.go
+++ titlesuploader/main.go
@@ -4,16 +4,36 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
+// Person struct for testing
 type Person struct {
 	Name  string
 	Phone string
 }
 
+func uploadToMongo(c *gin.Context) {
+
+}
+
 func main() {
+
+	router := gin.Default()
+	v1 := router.Group("/")
+	{
+		// v1.POST("/", createTodo)
+		// v1.GET("/", deleteTodo)
+		// v1.GET("/init", deleteTodo)
+		v1.POST("/uploadToMongo", uploadToMongo)
+		// v1.GET("/:id", fetchSingleTodo)
+		// v1.PUT("/:id", updateTodo)
+		// v1.DELETE("/:id", deleteTodo)
+	}
+	router.Run(":9000")
+
 	session, err := mgo.Dial("langdb:27017")
 	if err != nil {
 		panic(err)
